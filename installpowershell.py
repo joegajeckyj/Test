9c275e74f5cd0a68e2fce8e12084c4b835c2fd13
JoeGajeckyj <joe.gajeckyj@centurylink.com>
2019-03-08 16:25:53 +0000

    Update installpowershell.py

--- installpowershell.py
+++ installpowershell.py
@@ -4,4 +4,4 @@
 # curl <a href="https://packages.microsoft.com/config/rhel/7/prod.repo" target="_blank">https://packages.microsoft.com/config/rhel/7/prod.repo</a> | sudo tee /etc/yum.repos.d/microsoft.repo
  
 # Install PowerShell
-sudo yum install -y powershell
+yum install -y powershell
