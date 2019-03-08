158491d353a95394b17454cc07639d9ffea9252a
JoeGajeckyj <joe.gajeckyj@centurylink.com>
2019-03-08 16:52:40 +0000

    Update and rename installpowershell.py to powershelldocker.sh

--- installpowershell.py
+++
@@ -1,7 +0,0 @@
-#!/bin/python
-
-# Register the Microsoft RedHat repository
-# curl <a href="https://packages.microsoft.com/config/rhel/7/prod.repo" target="_blank">https://packages.microsoft.com/config/rhel/7/prod.repo</a> | sudo tee /etc/yum.repos.d/microsoft.repo
- 
-# Install PowerShell
-yum install -y powershell
---
+++ powershelldocker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Load Powershell
+docker pull mcr.microsoft.com/powershell:latest
+ 
+# test service
+get-service
+ 
