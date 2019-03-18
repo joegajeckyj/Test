736ced749c2b4139624250e2a81b59e5d911be9c
JoeGajeckyj <joe.gajeckyj@centurylink.com>
2019-03-18 15:48:11 +0000

    Update and rename zendeskreport.ps1 to zendeskreport.py

---
+++ zendeskreport.py
@@ -0,0 +1,46 @@
+import requests
+import json
+import sys
+
+urlSearchString = str(sys.argv[1])
+bearerToken = 'basic ' + str(sys.argv[2])
+
+print('Customer Search:', urlSearchString)
+
+proxies = {
+  'http': 'http://10.12.172.14:8080',
+  'https': 'https://10.12.172.14:8080'
+}
+
+headers = {
+  'Authorization': bearerToken
+}
+
+# Create the search URL
+url = 'https://t3n.zendesk.com/api/v2/search.json?query=' + urlSearchString + ' status<closed&sort_by=date&sort_order=desc'
+response = requests.get(url,headers=headers,proxies=proxies,verify=True)
+#print (response.status_code)
+# For successful API call, response code will be 200 (OK)
+if(response.ok):
+  # Loading the response data into a dict variable
+  casecount = response.json()['count']
+  print('Case Count:',(casecount))
+  #response.json()['results']  
+  # For each resault parse the ticket info
+  for result in list(response.json()['results']):
+    ticketID = (result['id'])
+    ticketSubject = (result['subject'])
+    ticketStatus = (result['status'])
+    ticketURL = (result['url'])
+    print((ticketID),"-",(ticketSubject),'- Status: ' + str(ticketStatus))
+    hyperlink = "<a href='https://t3n.zendesk.com/agent/tickets/" + str(ticketID) + "'>ticketID</a>"
+    pass
+else:
+  # If response code is not ok (200), print the resulting http error code with description
+  response.raise_for_status()
+  pass
+
+
+
+
+
